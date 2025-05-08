c4cc3953e8c522b318a89944c2ae216f8e0ccb70
Oscar gracia <113419592+OscarGrC@users.noreply.github.com>
2025-05-08 11:55:21 +0200

    layout finish and working

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { LayoutComponent } from './app/layout/layout.component';
+import { AppComponent } from './app/app.component';
 
-bootstrapApplication(LayoutComponent, {
+bootstrapApplication(AppComponent, {
   providers: [provideRouter(routes)]
 });
